eb8cb39f02a8060feb8fc74fc8c5580ee73268fa
ryanh1 <ryan.s.hollander@gmail.com>
2023-08-12 21:18:41 -0700

    Update ComicStrip.js

--- client/new-comic-front-end/src/components/ComicStrip.js
+++ client/new-comic-front-end/src/components/ComicStrip.js
@@ -25,14 +25,16 @@ const ComicStrip = ({
 
       {comicStrip.map((caption, index) => {
 
-      const path = `https://new-comic-fork-production.up.railway.app/server/${imagePaths[index].splice(0,6)}`;
-      console.log({path})
+      const imagePath = imagePaths[index];
+      console.log({imagePath});
+      const path = `https://new-comic-fork-production.up.railway.app/server/${imagePath.splice(0,6)}`;
+      console.log({path});
 
       return (
         <div className="flex flex-col justify-center items-center space-y-4" key={index}>
             <img 
               className="rounded w-72 h-72"
-              src={imagePaths[index]}
+              src={path}
               alt={index}
             />
             <div className="text-sm text-gray-700 w-48">{caption}</div>
