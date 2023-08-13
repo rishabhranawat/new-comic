d59974afea11603656cf7c4d10bc4fc2c92718d6
rishabhranawat <rishabhranawat12345@gmail.com>
2023-08-12 18:09:21 -0700

    return image paths

--- server/server.py
+++ server/server.py
@@ -9,6 +9,7 @@ from PIL import Image
 from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 import threading
+import uuid
 
 # app settings
 app = Flask(__name__)
@@ -25,6 +26,7 @@ STABILITY_AI_IMAGE_GEN_MODEL = 'stable-diffusion-xl-1024-v1-0'
 openai.api_key = OPEN_AI_API_KEY
 os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
 os.environ['STABILITY_KEY'] = STABILITY_AI_API_KEY
+COMIC_IMAGES_BASE_DIR = '~/news-comic/server/'
 
 # stability generation
 stability_api = client.StabilityInference(
@@ -34,6 +36,9 @@ stability_api = client.StabilityInference(
     # Check out the following link for a list of available engines: https://platform.stability.ai/docs/features/api-parameters#engine
 )
 
+def get_image_path(unique_id, scene_num):
+	return str(unique_id)+'_'+str(scene_num)+ ".png"
+
 ############################GPT##################################
 def get_gpt_response(content):
 	chat_completion = openai.ChatCompletion.create(
@@ -42,7 +47,7 @@ def get_gpt_response(content):
 	return chat_completion.choices[0].message.content
 
 def generate_comic_strip(content):
-	prompt = f'Convert this news story into a comic strip with 4-5 scene descriptions: {content}. \
+	prompt = f'Convert this news story into a comic strip with exactly 5 scene descriptions: {content}. \
 		Please keep the information factual.\
 		Keep the format in an ordered list. \
 		Just provide 1 line descriptions.'
@@ -54,13 +59,15 @@ def parse_comic_string_response(comic_strip_response):
 	return [x for x in comic_strip_response.split("\n")]
 
 ############################STABILITY##################################
-def generate_comic_strip_for_single_prompt(comic_scene_prompt, scene_num):
+def generate_comic_strip_for_single_prompt(comic_scene_prompt, scene_num, request_unique_id):
+	print(f'Comic Book prompt...{comic_scene_prompt}')
 	# Set up our initial generation parameters.
 	answers = stability_api.generate(
-			prompt=f'Create a comic scene like the following: {comic_scene_prompt}',
+			prompt=f'{comic_scene_prompt}',
+			style_preset='comic-book',
 			seed=4253978046,
 			steps=50,
-			cfg_scale=8.0,
+			cfg_scale=7.0,
 			width=1024, # Generation width, defaults to 512 if not included.
 			height=1024, # Generation height, defaults to 512 if not included.
 			samples=1, # Number of images to generate, defaults to 1 if not included.
@@ -75,22 +82,25 @@ def generate_comic_strip_for_single_prompt(comic_scene_prompt, scene_num):
 									"Please modify the prompt and try again.")
 					if artifact.type == generation.ARTIFACT_IMAGE:
 							img = Image.open(io.BytesIO(artifact.binary))
-							img.save(str(scene_num)+ ".png") # Save our generated images with their seed number as the filename.
+							img.save(get_image_path(request_unique_id, scene_num)) # Save our generated images with their seed number as the filename. 
 
-def generate_all_comic_scenes(comic_strip_response):
+def generate_all_comic_scenes(comic_strip_response, request_unique_id):
 		threads = []
-		
+
 		for scene_num, description in enumerate(comic_strip_response):
 				print(scene_num)
-				thread = threading.Thread(target=generate_comic_strip_for_single_prompt, args=(description,scene_num))
+				thread = threading.Thread(target=generate_comic_strip_for_single_prompt, 
+					args=(description,scene_num, request_unique_id))
 				threads.append(thread)
 				thread.start()
 
 		for thread in threads:
 				thread.join()
+		return [COMIC_IMAGES_BASE_DIR+get_image_path(request_unique_id, scene_num) for scene_num in range(len(comic_strip_response))]
 
 @app.route('/api/post', methods=['POST'])
 def post_endpoint():
+	request_unique_id = str(uuid.uuid4())
 	data = request.get_json()
 	if not data:
 		return jsonify({"message": "No input data provided"}), 400
@@ -100,11 +110,12 @@ def post_endpoint():
 	comic_strip = generate_comic_strip(data['content'])
 	comic_strip_response = parse_comic_string_response(comic_strip)
 
-	generate_all_comic_scenes(comic_strip_response)
+	image_paths = generate_all_comic_scenes(comic_strip_response, request_unique_id)
 
 	return jsonify(
 		{
 			"comic_strip": comic_strip_response,
+			"image_paths": image_paths
 		}), 201
 
 if __name__ == "__main__":
